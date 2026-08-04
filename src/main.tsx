59faab783c793cc859fc9835f2f56665c46dfd32
Grigorio Rodrigues de Lima Neto <rm102214@estudante.fieb.edu.br>
2026-08-04 18:59:48 -0300

    first elements

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client'
+import './global.css'
+import { Index } from './pages/index'
+
+createRoot(document.getElementById('root')!).render(
+    <Index />
+)
---
+++ src/pages/index/index.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+export const Index = (): React.JSX.Element => {
+    return (<>
+        <div className='div-index-context'>
+            <h1 className='h1-index-title'>Our Pricing</h1>
+            <div className='div- flex-row-stlye'>
+
+            </div>
+        </div>
+    </>)
+}
